9f7b39365dbd74f01af758a9e93863a1809c3b1d
John Rooey <JROOEY@mail.depaul.edu>
2020-10-20 23:04:10 -0400

    finished part 4

--- Midterm.py
+++ Midterm.py
@@ -84,6 +84,7 @@ cursor.executemany('INSERT INTO Student VALUES(?, ?, ?, ?);', students)
 cursor.executemany('INSERT INTO Course VALUES(?, ?, ?);', courses)
 cursor.executemany('INSERT INTO Grade VALUES(?, ?, ?);', grades)
 
+# 4. a
 view = ''' CREATE VIEW AllCols AS
     SELECT Student.StudentID, Name, Address, GradYear, Course.CName, CGrade, Department, Credits
     FROM Student LEFT OUTER JOIN Grade ON Student.StudentID = Grade.StudentID
@@ -99,11 +100,13 @@ AllCols = cursor.execute('SELECT * FROM AllCols;').fetchall()
 #     print(i)
 # cursor.execute('DROP VIEW AllCols;')
 
+# 4. b
 file = open('midterm.txt', 'w')
 for i in AllCols:
     file.write(str(i)[1:-1] + '\n')
 file.close()
 
+# 4. d
 file = open('midterm.txt', 'r')
 data = file.readlines()
 file.close()
@@ -118,3 +121,34 @@ for i in data:
             print('Error: CName -> Credits Not Consistent')
             print(i)
 
+# 4. e. 1
+for i in cursor.execute('SELECT Department, GradYear FROM AllCols GROUP BY Department;').fetchall():
+    print(i)
+
+# 4. e. 2
+dict = {}
+for i in data:
+    values = i.replace('\'', '').strip().split(', ')
+    if values[-2] not in dict.keys():
+        dict[values[-2]] = [values[3]]
+    else:
+        dict[values[-2]].append(values[3])
+
+for key, value in dict.items():
+    years = []
+    for i in value:
+        try:
+            years.append(int(i))
+        except:
+            continue
+    if len(years) == 0:
+        print(key, 'Null')
+    else:
+        print(key, min(years))
+
+
+# conn.row_factory = sqlite3.Row
+# cursor = conn.cursor()
+# tuples = []
+# for i in cursor.execute('SELECT * FROM AllCols').fetchall():
+#     tuples.append(tuple(i))
